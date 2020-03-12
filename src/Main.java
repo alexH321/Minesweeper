096954efd021df81c667a2b4b49abb062bee1cd0
alexH321 <Dorpsplein-8-kop>
2020-03-12 11:04:41 +0100

    1.5

--- src/Main.java
+++ src/Main.java
@@ -2,13 +2,33 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] arg){
-        //TODO: hoeveel rijen en kolommen
-        //TODO: hoeveel bommen
-        //TODO: board printen
-        //TODO: waardes berekenen
-        //TODO: eerste klik
-        //TODO: linker of rechterklik
-        rows = sc.hasNext();
-        yeet
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("How many rows do you want?"); //amount of rows
+        int rows =  sc.nextInt();
+
+        System.out.println("How many columns do you want?"); //amount of columns
+        int columns = sc.nextInt();
+
+        Board board = new Board(rows, columns);
+
+        int maximum = (rows*columns)/5;
+        System.out.println("How many bombs do you want? The maximum is: " + maximum); //maximum 1/5 van het bord?
+        int amountOfBombs = sc.nextInt(); //amount of bombs
+        board.setAmountOfBombs(amountOfBombs);
+        if (amountOfBombs <= maximum){
+            board.addBombs(amountOfBombs);
+        }
+        else{
+            System.out.println("There are too many bombs.")
+        }
+
+        board.calculateValues(); //calculate values
+
+        board.printBoard(); //print the board
+        //TODO: first click
+
+        //TODO: left or right click
+
     }
 }
